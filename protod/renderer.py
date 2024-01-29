15739c451ae031c3b6de6c12d205af6a7356fa4e
aj3423 <aj3423@gmail.com>
2024-01-30 00:53:46 +0800

    before reconstruct

--- protod/renderer.py
+++ protod/renderer.py
@@ -3,13 +3,9 @@ from html import escape
 
 from termcolor import colored
 
+
 # Chunk formatter and colorizer
 class Renderer(ABC):
-
-    # Add an cell to the final result
-    @abstractmethod
-    def add(self, cell):
-        pass
 
     # Return the final result, which can be in different formats, eg:
     # for console, it's a string with ansi color
@@ -20,7 +16,7 @@ class Renderer(ABC):
 
     # normal white cell
     @abstractmethod
-    def add_normal(self, s:str):
+    def add_normal(self, s: str):
         pass
 
     # the indent character like '\t' or '&nbsp;'
@@ -48,12 +44,12 @@ class Renderer(ABC):
     def add_type(self, s: str):
         pass
 
-    # add numbers
+    # add a number value
     @abstractmethod
     def add_num(self, s: str):
         pass
 
-    # add strings
+    # add a string value
     @abstractmethod
     def add_str(self, s: str):
         pass
@@ -63,94 +59,97 @@ class Renderer(ABC):
     def add_bin(self, s: memoryview):
         pass
 
+
 class ConsoleRenderer(Renderer):
 
-    # Long binary data that exceeds `n` bytes are truncated and followed by a '...' 
+    # Long binary data that exceeds `n` bytes are truncated and followed by a '...'
     # use a large value like 1000000 to 'not' truncate
     # default: 32
-    def __init__(self, truncate_after = 32):
-        self.cells = [] 
+    def __init__(self, truncate_after=32):
+        self.cells = []
         self.truncate_after = truncate_after
 
-    def add(self, cell):
+    def _add(self, cell):
         self.cells.append(cell)
         pass
 
     def build_result(self):
-        return ''.join(self.cells)
+        return "".join(self.cells)
 
     def add_indent(self, level):
         for _ in range(level):
-            self.add(' ' * 4)
+            self._add(" " * 4)
 
     def add_newline(self):
-        self.add("\n")
+        self._add("\n")
 
     def add_normal(self, s):
-        self.add(s)
+        self._add(s)
 
     def add_idtype(self, s):
-        self.add(colored(s, "light_red"))
+        self._add(colored(s, "light_red"))
 
     def add_id(self, s):
-        self.add(colored(s, "light_green"))
+        self._add(colored(s, "light_green"))
 
     def add_type(self, s):
-        self.add(colored(s, "yellow"))
+        self._add(colored(s, "yellow"))
 
     def add_num(self, s):
-        self.add(colored(s, "light_cyan"))
+        self._add(colored(s, "light_cyan"))
 
     def add_str(self, s):
-        self.add(colored(s, "light_blue"))
+        self._add(colored(s, "light_blue"))
 
     def add_bin(self, s):
-        truncated = s[:self.truncate_after]
-        self.add(colored(' '.join(format(x, '02x') for x in truncated), "light_yellow"))
+        truncated = s[: self.truncate_after]
+        self._add(
+            colored(" ".join(format(x, "02x") for x in truncated), "light_yellow")
+        )
         if len(s) > self.truncate_after:
-            self.add_normal(' ...')
+            self.add_normal(" ...")
 
 
-# The HtmlRenderer builds a full html div string, 
+# The HtmlRenderer builds a full html div string,
 # which can be simply set to a <div>
 class HtmlRenderer(Renderer):
 
     def __init__(self):
-        self.cells = [] 
+        self.cells = []
 
-    def add(self, cell):
+    def _add(self, cell):
         self.cells.append(cell)
         pass
 
     def build_result(self):
-        return ''.join(self.cells)
+        return "".join(self.cells)
 
     def add_indent(self, level):
         for _ in range(level):
-            self.add("&nbsp;" * 4)
+            self._add("&nbsp;" * 4)
 
     def add_newline(self):
-        self.add("</br>")
+        self._add("</br>")
 
     def add_normal(self, s):
-        self.add(s)
+        self._add(s)
 
     def add_idtype(self, s):
-        self.add(f"<font color='#ff2200'>{s}</font>")
+        self._add(f"<font color='#ff2200'>{s}</font>")
 
     def add_id(self, s):
-        self.add(f"<font color='#00ff11'>{s}</font>")
+        self._add(f"<font color='#00ff11'>{s}</font>")
 
     def add_type(self, s):
-        self.add(f"<font color='#808000'>{s}</font>")
+        self._add(f"<font color='#808000'>{s}</font>")
 
     def add_num(self, s):
-        self.add(f"<font color='cyan'>{s}</font>")
+        self._add(f"<font color='cyan'>{s}</font>")
 
     def add_str(self, s):
-        self.add(f"<font color='blue'>{escape(s)}</font>")
+        self._add(f"<font color='blue'>{escape(s)}</font>")
 
     def add_bin(self, s):
-        self.add(f"<font color='yellow'>{escape(' '.join(format(x, '02x') for x in s))}</font>")
-
-
+        self._add(
+            f"<font color='yellow'>{escape(' '.join(format(x, '02x') for x in s))}</font>"
+        )
